6814d8d58aa0c327c639164f073be72c5bd8a43c
Bastiaan Quast <bquast@gmail.com>
2023-11-11 05:57:01 -0600

    take CoefMod from HEtools

--- R/primitives.R
+++ R/primitives.R
@@ -11,21 +11,9 @@ GenPolyMod <- function(n)
   polynomial( coef=c(1, rep(0, n-1), 1 ) )
 
 
-#' @name CoefMod
-#' @title Coefficient Modulo
-#' @param x polynomial from the polynom package
-#' @param k the modulo
-#' @return polynomial of the polynom class
-#' @import polynom
+#' @importFrom HEtools CoefMod
 #' @export
-#' @examples
-#' polynomial = polynomial(c(5, 3, 6))
-#' print(polynomial)
-#'
-#' CoefMod(polynomial, 5)
-CoefMod <- function(x, k)
-  polynom::polynomial(as.vector(x)%%k)
-
+HEtools::CoefMod
 
 #' @name GenSecretkey
 #' @title Generate Secret key
